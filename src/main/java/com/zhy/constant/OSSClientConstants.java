9f739456fa8f4571d2d41313c0c3c1a8be69a704
pengliu <iversionpeng@163.com>
2019-05-27 10:36:52 +0800

    初始版本

--- src/main/java/com/zhy/constant/OSSClientConstants.java
+++ src/main/java/com/zhy/constant/OSSClientConstants.java
@@ -12,45 +12,40 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "lp.oss")
 public class OSSClientConstants {
 
-    private static String accessKeyId;
-    private static String accessKeySecret;
-    private static String backetName;
-
-
-    public void setAccessKeyId(String accessKeyId) {
-        OSSClientConstants.accessKeyId = accessKeyId;
-    }
-
-    public void setAccessKeySecret(String accessKeySecret) {
-        OSSClientConstants.accessKeySecret = accessKeySecret;
-    }
-
-    public void setBacketName(String backetName) {
-        OSSClientConstants.backetName = backetName;
-    }
-
     /**
      * 阿里云API的外网域名
      */
     public static final String ENDPOINT = "oss-cn-beijing.aliyuncs.com";
-
     /**
      * 阿里云API的密钥Access Key ID
      */
-    public static final String ACCESS_KEY_ID = accessKeyId;
+    public static String ACCESS_KEY_ID;
     /**
      * 阿里云API的密钥Access Key Secret
      */
-    public static final String ACCESS_KEY_SECRET = accessKeySecret;
+    public static String ACCESS_KEY_SECRET;
 
     /**
      * 阿里云API的bucket名称
      */
-    public static final String BACKET_NAME = backetName;
-
+    public static String BACKET_NAME;
     /**
      * 阿里云API的文件夹名称
      */
     public static final String FOLDER = "public/";
+
+
+    public void setAccessKeyId(String accessKeyId) {
+        OSSClientConstants.ACCESS_KEY_ID = accessKeyId;
+    }
+
+    public void setAccessKeySecret(String accessKeySecret) {
+        OSSClientConstants.ACCESS_KEY_SECRET = accessKeySecret;
+    }
+
+    public void setBacketName(String backetName) {
+        OSSClientConstants.BACKET_NAME = backetName;
+    }
+
 
 }
